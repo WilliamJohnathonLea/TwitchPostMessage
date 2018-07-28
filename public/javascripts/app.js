df4c2a390a4d5d06d85fb7868bbcbacbe456d5e2
William Lea <wjlea93@gmail.com>
2018-07-28 14:10:14 +0100

    Showing users that something is happening

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
+    var form = $('form');
+    var submitBtn = $('#btn-submit');
 
+    submitBtn.on('click', function(){
+        submitBtn.attr('disabled', 'disabled');
+        submitBtn.html('Processing...');
+        form.submit();
+    });
 });
